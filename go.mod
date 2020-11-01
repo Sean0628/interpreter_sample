ffedc3a09625009706df218afad5999803574dde
Sho ITO <i.sho.0628@gmail.com>
2020-11-01 20:07:33 +0900

    Add New function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sean0628/interpreter_sample
+
+go 1.15
---
+++ lexer/lexer_test.go
@@ -0,0 +1,35 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/Sean0628/interpreter_sample/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType   token.TokenType
+		expecteLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+	}
+}

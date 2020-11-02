99c4a212eee4eac16ac1a972cfb537572e4eee37
Sho ITO <i.sho.0628@gmail.com>
2020-11-02 23:12:31 +0900

    Add first NextToken function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,10 +1,47 @@
 package lexer
 
+import(
+	"github.com/Sean0628/interpreter_sample/token"
+)
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
 	readPosition int  // current reading points in input (after current char)
 	ch           byte // current char under examination
+}
+
+func (l *Lexer) NextToken() token.Token {
+  var tok token.Token
+
+  switch l.ch {
+  case '=':
+    tok = newToken(token.ASSIGN, l.ch)
+  case ';':
+    tok = newToken(token.SEMICOLON, l.ch)
+  case '(':
+    tok = newToken(token.LPAREN, l.ch)
+  case ')':
+    tok = newToken(token.RPAREN, l.ch)
+  case ',':
+    tok = newToken(token.COMMA, l.ch)
+  case '+':
+    tok = newToken(token.PLUS, l.ch)
+  case '{':
+    tok = newToken(token.LBRACE, l.ch)
+  case '}':
+    tok = newToken(token.RBRACE, l.ch)
+  case 0:
+    tok.Literal = ""
+    tok.Type = token.EOF
+  }
+
+  l.readChar()
+  return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+  return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 func New(input string) *Lexer {

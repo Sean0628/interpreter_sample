2dae0667a382157cded9172c0dc88371b910a8d9
Sho ITO <i.sho.0628@gmail.com>
2020-11-03 21:01:23 +0900

    Add some one-charactoer tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -43,6 +43,9 @@ func TestNextToken_BasicAssignment(t *testing.T) {
   };
 
   let result = add(five, ten);
+
+  !-/*5;
+  5 < 10 > 5;
   `
 	tests := []struct {
 		expectedType   token.TokenType
@@ -83,6 +86,18 @@ func TestNextToken_BasicAssignment(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
